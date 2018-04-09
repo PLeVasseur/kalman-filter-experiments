6a9730c96044b426d4e12f75696e777aed7bafb6
Peter LeVasseur <plevasseur@gmail.com>
2018-04-09 06:41:06 -0400

    Basic example of creating a KalmanFilterMatGen with not much implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate rudie;
+extern crate typenum;
+
+use rudie::KalmanFilterMatGen2;
+use typenum::consts::*;
+
 fn main() {
+    let kf_mg: KalmanFilterMatGen2<f32, U6> = KalmanFilterMatGen2::init();
+
+    println!("{:?}", kf_mg);
+
+
     println!("Hello, world!");
+
+
 }
